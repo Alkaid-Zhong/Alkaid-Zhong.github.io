23b7000b1efedfd76882fc0c6c2fb6b670eaa195
钟昊翔 <21231025@buaa.edu.cn>
2024-07-11 14:19:27 +0800

    test github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://alkaid-zhong.github.io/",
-  base: '/alkaid-zhong.github.io/',
+  base: '/alkaid-zhong/',
   trailingSlash: "always",
   integrations: [
     tailwind(),
